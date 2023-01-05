0a4c2f0b409f6268f5133b56863efaff7cf5a0c8
Anshul Mankotia <anshulmankotia1997@gmail.com>
2023-01-05 12:09:19 +0530

    my final submition

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,30 +1,16 @@
 
 import "./Intro.css";
-import Vector1 from "../../images/Vector1.png";
-import Vector2 from "../../images/Vector2.png";
-import boy from "../../images/me.png";
-import glassesimoji from "../../images/glassesimoji.png";
-import thumbup from "../../images/thumbup.png";
-import crown from "../../images/crown.png";
-import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import boy from "../..//images/me.png";
 import Github from "../../images/github.png";
 import LinkedIn from "../../images/linkedin.png";
 import Instagram from "../../images/instagram.png";
-import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 const Intro = () => {
-  // Transition
-  const transition = { duration: 2, type: "spring" };
-
-  // context
-
-
   return (
     <div className="Intro" id="Intro">
       {/* left name side */}
       <div className="i-left">
         <div className="i-name">
-          {/* yahan change hy darkmode ka */}
           <span>Hy! I Am</span>
           <span>Anshul Mankotia</span>
           <span>
@@ -46,38 +32,8 @@ const Intro = () => {
       </div>
       {/* right image side */}
       <div className="i-right">
-        <img src={Vector1} alt="" />
-        <img src={Vector2} alt="" />
+       
         <img src={boy} alt="" />
-        {/* animation */}
-        <motion.img
-          initial={{ left: "-36%" }}
-          whileInView={{ left: "-24%" }}
-          transition={transition}
-          src={glassesimoji}
-          alt=""
-        />
-
-        <motion.div
-          initial={{ top: "-4%", left: "68%" }}
-          whileInView={{ left: "51%" }}
-          transition={transition}
-          className="floating-div"
-        >
-          <FloatinDiv img={crown} text1="Web" text2="Developer" />
-        </motion.div>
-
-        {/* animation */}
-        <motion.div
-          initial={{ left: "9rem", top: "22rem" }}
-          whileInView={{ left: "0rem" }}
-          transition={transition}
-          className="floating-div"
-        >
-          {/* floatinDiv mein change hy dark mode ka */}
-          <FloatinDiv img={thumbup} text1="Web" text2="Designing"/>
-        </motion.div>
-
         <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
         <div
           className="blur"
